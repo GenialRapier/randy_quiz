4a7eb1654e5ad1a1ae91d9902a065506bb70db9d
ahmad.windardi80 <ahmad.windardi80@gmail.com>
2018-11-13 08:46:26 +0700

    routing

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 
+	"github.com/gorilla/mux"
 )
 
-
 func main() {
+	r := mux.NewRouter()
 
+	r.HandleFunc("/test", testServer)
+
+	r.HandleFunc("/addItem", testServer).Methods("GET")
+	r.HandleFunc("/removeItem", testServer).Methods("GET")
+	r.HandleFunc("/editItem", testServer).Methods("GET")
+	r.HandleFunc("/deleteItem", testServer).Methods("GET")
+
+	r.HandleFunc("/addCategory", testServer).Methods("GET")
+	r.HandleFunc("/removeCategory", testServer).Methods("GET")
+	r.HandleFunc("/editCategory", testServer).Methods("GET")
+	r.HandleFunc("/deleteCategory", testServer).Methods("GET")
+
+	http.ListenAndServe(":14045", r)
+}
+
+func testServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Server is Up")
 }
